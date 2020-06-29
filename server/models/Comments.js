edd7d5ddfad21da34dd2303d8c26fbe834d20526
Owen Jones <owenthefirst93@gmail.com>
2020-06-29 15:47:27 -0400

    comments rt and model finished

--- server/models/Comments.js
+++ server/models/Comments.js
@@ -9,3 +9,25 @@ getCommentsByAlbumId = async (album_id) => {
         console.log('err', error)
     }
 }
+
+addNewComment = async (comment) => {
+    try{
+        insertQuery = `INSERT INTO comments(comment_body, user_id, commenter_id, album_id) VALUES($/comment_body/,$/user_id/, $/commenter_id/, $/album_id/)`
+
+        const newComment = await db.any(insertQuery, {
+            comment_body: comment.comment_body,
+            user_id: comment.user_id,
+            commenter_id: comment.commenter_id,
+            album_id: comment.album_id
+        })
+        return newComment
+    }catch(error){
+        console.log('err', error)
+    }
+}
+
+
+module.exports = {
+    getCommentsByAlbumId,
+    addNewComment
+}
--- server/routes/comments.js
+++ server/routes/comments.js
@@ -1,17 +1,52 @@
 const express = require('express');
 const router = express.Router();
-
+const Comments = require('../models/Comments')
 
 router.get('/', (req, res, next) =>{
     res.send('users rt: Refer to the README on how to navigate routes')
 })
 
-router.get('/album/:id', (req, res, next) => {
+router.get('/album/:id', async (req, res, next) => {
+    const {id} = req.params
     try{
-        
+        const albumCommentsById = await Comments.getCommentsByAlbumId(id)
+        res.json({
+            payload: albumCommentsById,
+            msg: `Getting all comments from album # ${id}`,
+            err: false
+        })
     }catch(error){
-        
+        console.log('err', error)
+        res.json({
+            msg: 'Failed to get comments',
+            err: true
+        })
     }
 })
+
+router.post('/', async (req, res, next) => {
+    const {comment_body, user_id, commenter_id, album_id} = req.body
+    try{
+        let comment = {
+            comment_body, 
+            user_id,
+            commenter_id, 
+            album_id
+        }
+        const newComment = await Comments.addNewComment(comment)
+        res.json({
+            payload: newComment,
+            msg: 'Comment posted. Success',
+            err: false
+        })
+    }catch(error){
+        console.log('err', error)
+        res.json({
+            msg: 'Something went wrong. Posting failed',
+            err: true
+        })
+    }
+})
+
 
 module.exports = router 
