7f7e35849d09dff9ef8aef62e07448ce9afd99dd
Owen Jones <owenthefirst93@gmail.com>
2020-07-08 16:07:07 -0400

    app js front end and nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,38 @@
 import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom'
 import NavBar from './Components/Navigation/NavBar.jsx'
-
+import LandingPage from './Components/Home/LandingPage.jsx'
+import ProfilePage from './Components/ProfilePage/Profile.jsx'
 import './App.css';
 
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      user : null,
+      userId: null,
       isUserLoggedIn: false,
       wasInitialized: false
     }
   }
+   renderHomePage = (routeProps) => {
+    return <LandingPage {...routeProps} />
+  }
+
+  renderProfilePage = (routeProps) => {
+    return <ProfilePage {...routeProps} userId={this.state.userId}/>
+  }
+
   render(){
     return (
       <div>
         <h1>Hello World</h1>
         <NavBar />
+        <div> 
+          <Switch>
+            <Route exact path= "/" render={this.renderHomePage}/>
+            <Route path="/profile/:id" render={this.renderProfilePage}/>
+          </Switch>
+        </div>
       </div>
     )
   }
---
+++ client/src/Components/Home/LandingPage.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+
+class LandingPage extends Component {
+    constructor(){
+        super()
+        this.state = {
+            userId: null
+        }
+    }
+    render(){
+        return(
+            <div>
+                <h2>Landing Page</h2>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+export default LandingPage
