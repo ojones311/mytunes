2e510a142e6d0cee953806e2763eef7822668835
Owen Jones <owenthefirst93@gmail.com>
2020-07-09 20:34:44 -0400

    spotify credentials

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
 const app = express();
 
 const PORT = 8000;
@@ -16,6 +17,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', homeRouter);
 app.use('/users', usersRouter);
