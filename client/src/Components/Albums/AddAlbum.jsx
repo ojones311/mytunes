6132d049bcae532684d126776d8668008ec8c15e
Owen Jones <owenthefirst93@gmail.com>
2020-07-23 15:28:18 -0400

    handling album and artist search

--- client/src/Components/Albums/AddAlbum.jsx
+++ client/src/Components/Albums/AddAlbum.jsx
@@ -6,38 +6,39 @@ class AddAlbum extends Component {
         super(props)
         this.state = {
             userId: props.userId,
+            credentials: '',
             searchBarValue: '',
             searchResults: [],
             submittedSearch: false,
             credentialsNeeded: true,
             config: props.config,
             data: props.data,
-            searchType:''
+            searchType: 'album'
         }
     }
 
     componentDidMount = async () => {
         // this.props.getSpotifyCredentials()
+        // await this.updateCredentials()
+        // await this.getSpotifyId()
     }
 
-    // evaluateCredentials = async() => {
-    //     const {config, data} = this.state
-    //     console.log('config =>', config)
-    //     console.log('data =>', data)
-    //     this.props.getSpotifyCredentials()
-    // }
-
-    handleSearch = async(event) => {
+    handleAlbumSearch = async(event) => {
         event.preventDefault()
         if(this.isFormCompleted()){
             console.log('Search pending')
             this.setState({
                 submittedSearch: true
             })
+            this.getSpotifyId()
+
             this.clearSearchBar()
         }else{
             console.log('Complete search to continue')
         }
+    }
+    handleArtistSearch =  async(event) => {
+        
     }
     handleSearchValue = async (event) => {
         this.setState({
@@ -57,22 +58,36 @@ class AddAlbum extends Component {
             searchBarValue: ''
         })
     }
+    updateCredentials = async() => {
+        const {config, data} = this.state
+        // console.log('config =>', config)
+        // console.log('data =>', data)
+        let credentials = await this.props.getSpotifyCredentials()
+        console.log(credentials)
+        this.setState({
+            credentials: credentials
+        })
+    }
     getSpotifyId = async () => {
+        const {credentials} = this.state
+        console.log(credentials)
         try{
-            let response = await axios.get() 
+            let response = await axios.get('https://api.spotify.com/v1/search', {headers: {Authorization: credentials.token_type + '' + credentials.access_token}})
+            console.log('res = >', response) 
         }catch(error){
             console.log('err', error)
         }
     }
 
-    getAlbumFromSpotify =  async (token, type) => {
-        try{
-            let response = await axios.get(`https://api.spotify.com/v1/albums/${'392p3shh2jkxUxY2VHvlH8'}`, {headers: {Authorization: type + '' +token}})
-            console.log(response.data)
-        }catch(error){
-            console.log('err', error)
-        }
-    }
+    // getAlbumFromSpotify =  async () => {
+    //     const {config, data} = this.state
+    //     try{
+    //         let response = await axios.get(`https://api.spotify.com/v1/albums/${'392p3shh2jkxUxY2VHvlH8'}`, {headers: {Authorization: type + '' +token}})
+    //         console.log(response.data)
+    //     }catch(error){
+    //         console.log('err', error)
+    //     }
+    // }
     searchByArtist = () => {
         this.setState({
             searchType: 'artist'
@@ -91,8 +106,8 @@ class AddAlbum extends Component {
             return(
                 <div>
                     <h2>Search by artist or album </h2>
-                    <button onClick={this.searchCriteriaChange}>Search by artist</button>
-                    <button onClick={}>Search by album name</button>
+                    <button onClick={this.searchByArtist}>Search by artist</button>
+                    <button onClick={this.searchByAlbum}>Search by album name</button>
                     <div>
                         <form onSubmit={this.handleSearch}>
                             <input id='album-search' type='text' placeholder='Enter album name' size={'50'} onChange={this.handleSearchValue} value={searchBarValue}></input>
@@ -102,7 +117,19 @@ class AddAlbum extends Component {
                 </div>
             )
         }else if(searchType === 'artist'){
-
+            return(
+                <div>
+                    <h2>Search by artist or album </h2>
+                    <button onClick={this.searchByArtist}>Search by artist</button>
+                    <button onClick={this.searchByAlbum}>Search by album name</button>
+                    <div>
+                        <form onSubmit={this.handleSearch}>
+                            <input id='album-search' type='text' placeholder='Enter artist name' size={'50'} onChange={this.handleSearchValue} value={searchBarValue}></input>
+                            <button type='submit'>Search</button>
+                        </form>
+                    </div>
+                </div>
+            )
         }      
     }
 }
